d8068d7ecdbea11f19eb414e5450b8ae53aa0f21
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-30 19:42:52 +0200

    LOCAL_KEY

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { Section } from './App.styles';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from '../ContactForm/ContactForm ';
 import { ContactList } from '../ContactList/ContactList';
@@ -7,32 +7,22 @@ import { Filter } from '../Filter/Filter';
 
 const LOCAL_KEY = 'Users-key';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     const UsersKeys = localStorage.getItem(LOCAL_KEY);
-    if (UsersKeys) {
-      this.setState({ contacts: JSON.parse(UsersKeys) });
-    }
-  }
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
+    setContacts({ contacts: JSON.parse(UsersKeys) });
+  }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
-      localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
-    }
-  }
-
-  formSubmitHandler = data => {
+  const formSubmitHandler = data => {
     const newUser = {
       id: nanoid(),
       ...data,
     };
-    if (this.state.contacts.some(ele => ele.name === data.name)) {
+    if (setContacts.some(ele => ele.name === data.name)) {
       alert(`${data.name} is already in contacts!`);
       return;
     } else {
@@ -44,46 +34,104 @@ export class App extends Component {
     }
   };
 
-  filterUsers = event => {
-    this.setState({ filter: event.target.value });
+  const filterUsers = event => {
+    setFilter({ filter: event.target.value });
   };
 
-  verification = () => {
-    const { filter, contacts } = this.state;
-    if (!filter) {
-      return contacts;
-    } else {
-      return contacts.filter(
-        user =>
-          user.name.toLowerCase().includes(filter.toLowerCase()) ||
-          user.number.includes(filter)
-      );
-    }
-  };
+  return (
+    <>
+      <Section>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={formSubmitHandler} />
+        <h2>Contacts</h2>
+        <Filter filter={setFilter} click={filterUsers} />
 
-  deleteUsers = userId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(user => user.id !== userId),
-      };
-    });
-  };
+        {/* <ContactList
+          contacts={this.verification()}
+          deleteUsers={this.deleteUsers}
+        /> */}
+      </Section>
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <Section>
-          <h1>Phonebook</h1>
-          <ContactForm onSubmit={this.formSubmitHandler} />
-          <h2>Contacts</h2>
-          <Filter filter={this.state.filter} click={this.filterUsers} />
+// export class OldApp extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
 
-          <ContactList
-            contacts={this.verification()}
-            deleteUsers={this.deleteUsers}
-          />
-        </Section>
-      </>
-    );
-  }
-}
+// componentDidMount() {
+//   const UsersKeys = localStorage.getItem(LOCAL_KEY);
+//   if (UsersKeys) {
+//     this.setState({ contacts: JSON.parse(UsersKeys) });
+//   }
+// }
+
+// componentDidUpdate(prevProps, prevState) {
+//   const { contacts } = this.state;
+//   if (prevState.contacts !== contacts) {
+//     localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
+//   }
+// }
+
+// formSubmitHandler = data => {
+//   const newUser = {
+//     id: nanoid(),
+//     ...data,
+//   };
+//   if (this.state.contacts.some(ele => ele.name === data.name)) {
+//     alert(`${data.name} is already in contacts!`);
+//     return;
+//   } else {
+//     this.setState(prevState => {
+//       return {
+//         contacts: [...prevState.contacts, newUser],
+//       };
+//     });
+//   }
+// };
+
+// filterUsers = event => {
+//   this.setState({ filter: event.target.value });
+// };
+
+// verification = () => {
+//   const { filter, contacts } = this.state;
+//   if (!filter) {
+//     return contacts;
+//   } else {
+//     return contacts.filter(
+//       user =>
+//         user.name.toLowerCase().includes(filter.toLowerCase()) ||
+//         user.number.includes(filter)
+//     );
+//   }
+// };
+
+// deleteUsers = userId => {
+//   this.setState(prevState => {
+//     return {
+//       contacts: prevState.contacts.filter(user => user.id !== userId),
+//     };
+//   });
+// };
+
+//   render() {
+//     return (
+//       <>
+//         <Section>
+//           <h1>Phonebook</h1>
+//           <ContactForm onSubmit={this.formSubmitHandler} />
+//           <h2>Contacts</h2>
+//           <Filter filter={this.state.filter} click={this.filterUsers} />
+
+//           <ContactList
+//             contacts={this.verification()}
+//             deleteUsers={this.deleteUsers}
+//           />
+//         </Section>
+//       </>
+//     );
+//   }
+// }
