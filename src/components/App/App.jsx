c0f0370c10c19544810d54616c36db01339d7519
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-31 18:55:56 +0200

    useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,12 @@ import { Filter } from '../Filter/Filter';
 // const LOCAL_KEY = 'Users-key';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
   const [filter, setFilter] = useState('');
 
   // useEffect(() => {
@@ -34,12 +39,14 @@ export const App = () => {
   };
 
   const verification = () => {
-    if (!filter) {
+    console.log(filter);
+
+    if (filter) {
       return contacts;
     } else {
       return contacts.filter(
         user =>
-          user.name.toLowerCase().includes(filter.toLowerCase()) ||
+          user.name.toLowerCase().includes(filter.toLowerCase()) ??
           user.number.includes(filter)
       );
     }
@@ -55,7 +62,7 @@ export const App = () => {
         <h1>Phonebook</h1>
         <ContactForm onSubmit={formSubmitHandler} />
         <h2>Contacts</h2>
-        <Filter filter={setFilter} click={filterUsers} />
+        <Filter filter={filterUsers} click={filterUsers} />
 
         <ContactList contacts={verification()} deleteUsers={deleteUsers} />
       </Section>
